2892466c2305c9c8921beec595ac7eb728914d34
wetdogman <61250683+wetdogman@users.noreply.github.com>
2023-04-24 17:32:07 -0400

    possible last commit before presentation, everything working except password hasing and personal information

--- chris_kramer_lionAuction/README.txt
+++ chris_kramer_lionAuction/README.txt
@@ -1,5 +1,13 @@
-User login screen for LionAuctin.
+LionAuction Prototype V1
 
-User can log in with username and password.
+i) The beginning of the code is reading in the many datasets used to make the prototype work. This includes but is not limited to: User data, auction data, seller data, address and zipcode data, bidding history, etc.
 
-Do not enter a fake username and password.
+The code includes routes for each pages that handles their specific functionality as requested in the proposal, such as logging in securely, listing the many different auctions, filtering the many different auctions by category, placing bids on various auctions, viewing bidding history, etc.
+
+The html code lays out the foundation for each page, and is then filled with the necessary data from our stored pandas dataframes through the route functions. Pages include brief css styling at the top in order to make each page presentable and visually appealing to the user.
+
+ii) The features list of the prototype includes but is not limited to: Logging in securely as either a bidder or a seller, viewing ones own information, viewing the verious listings as a bidder, viewing the various bids placed as a bidder, filtering auctions by specific cateogories as a bidder, viewing the auctions you are hosting as a seller, and removing any auction you'd like to take down as a seller.
+
+iii) The main app.py file can be found in the main folder, and each html file can be found in the 'templates' folder within the primary chris_kramer_lionAuction folder. Within the main folder you can also find the README file and each dataset that was used for the purposes of demonstrating the functionality of this prototype.
+
+iv) In order to run the program, you must load the project folder into PyCharm Professional as a whole, which will open each individual file as will in the project manager screen located on the left of your UI. Assuming you've opened the project folder correctly, and can see all the necessary files within your project manager window, all you need to do is run the app.py file. And remember, sometimes these files take a long time to run, so be patient!
--- chris_kramer_lionAuction/app.py
+++ chris_kramer_lionAuction/app.py
@@ -1,11 +1,12 @@
+# importing the many packages we need for the project
 import csv
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import pandas as pd
 import math
-import hashlib
+import bcrypt
 
+# starting flask app
 app = Flask(__name__)
-app.secret_key = 'secretkey123'
 
 # Read in Auction_Listings.csv and create pandas dataframe
 auction_listings = pd.read_csv('Auction_Listings.csv', header=0)
@@ -15,17 +16,18 @@ categories = set(categories_df['category_name'].unique())
 # read in the bidding history
 bids_df = pd.read_csv("Bids.csv", header=0)
 
+# next long chunk is taking in all bidder information and merging it into one table
 with open('Address.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     headers = next(csvreader)  # Extract the header row
 
-# Read in all the data tables and merge them into a single data table
 with open('Address.csv') as file:
     address_table = list(csv.DictReader(file))
 
 with open('Bidders.csv') as file:
     bidder_table = list(csv.DictReader(file))
 
+# creating the merged table
 merged_table = []
 for bidder in bidder_table:
     home_address_id = bidder['home_address_id']
@@ -53,8 +55,7 @@ for bidder in bidder_table:
 # turn it into a pandas dataframe
 df = pd.DataFrame(merged_table)
 
-# Read in Auction_Listings.csv and create pandas dataframe
-
+# read in the user's emails and passwords
 with open('Users.csv') as file:
     users_table = list(csv.DictReader(file))
 
@@ -63,69 +64,82 @@ with open('Sellers.csv') as file:
     sellers_table = list(csv.DictReader(file))
 
 
-# Helper function to securely hash the passwords
+# hashes the passwords
 def hash_password(password):
-    return password
+    # encode the password string as bytes using utf-8, common method
+    password_bytes = password.encode('utf-8')
+    # generate a salt to use for the password hash
+    salt = bcrypt.gensalt()
+    # hash the password using the salt
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+    # Return the hashed password as a bytes object
+    return hashed_password
+
+
+# matches the passwords to check if they are the same
+# allows us to match entered password to encrypted password
+def match_password(password, hashed_password):
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
+
+# this code hashes every password in the Users.csv file, takes very long time to run
+# for row in users_table:
+#   row['password'] = hash_password(row['password'])
 
 
 # Route for the login page
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    if request.method == 'POST': # takes in the email and password they entered
         email = request.form['email']
         password = request.form['password']
         option = request.form['type']
 
-        if option == 'bidder':
+        if option == 'bidder': # if they are logging in as a bidder
             # Check if the email and password match a user in the users table
             for user in users_table:
-                if user['ï»¿email'] == email and user['password'] == hash_password(password):
-                    print("true dat")
-                    session['email'] = email
+                if user['ï»¿email'] == email and user['password'] == password:
+                    session['email'] = email # setting the universal log in email
                     flash('Logged in successfully.', 'success')
-                    print("bidder spotted")
                     return redirect(url_for('bidder', email=email))
 
-        # when checking for seller, we don't look for the email in the sellers.csv file because the emails are all different
-        # some are similar, but have a different @ address, so we cannot match them up
+        # check to make sure they are a seller, with their email being in Sellers.csv
         elif option == 'seller':
             # Redirect to the seller page
             for user in sellers_table:
                 if user['ï»¿email'] == email:
-                    print("got em")
-                    session['email'] = email
+                    session['email'] = email # setting the global session email for later use
                     flash('Logged in successfully.', 'success')
-                    print("seller spotted")
                     return redirect(url_for('seller', email=email))
 
         # If no matching user was found, show an error message
         return render_template('login.html', error='Wrong email or password')
 
     # If the request method is GET, just show the login page
-    return render_template('login.html')
+    return render_template('login.html') # if they fail to log in
 
 
-@app.route('/bidder', methods=['GET', 'POST'])
+@app.route('/bidder', methods=['GET', 'POST']) # first route if they are a bidder
 def bidder():
-    if request.method == 'POST' and 'search' in request.form:
+    if request.method == 'POST' and 'search' in request.form: # lists all the auctions in selected category
         return render_template('auctions.html', auction_listings=auction_listings[
             ['Product_Name', 'Product_Description', 'Quantity', 'Seller_Email']])
-    else:
+    else: # if they want to display their own personal information
         email = request.args.get('email')
-        bidder_info = df.loc[df['Owner_Email'] == email]
+        bidder_info = df.loc[df['Owner_email'] == email]
         bidder_info = pd.DataFrame(bidder_info)
         bidder_info_html = bidder_info.to_html(index=False)  # call to_html() on the DataFrame
         return render_template('bidder.html', bidder_info=bidder_info_html, email=email)
 
 
-@app.route('/auctions', methods=['GET', 'POST'])
+@app.route('/auctions', methods=['GET', 'POST']) # route for viewing filtered auctions
 def auctions():
     if request.method == 'POST':
         selected_category = request.form.get('category')
         print(selected_category)
-        if selected_category == 'All':
+        if selected_category == 'All': # if they want to view all auctions
             filtered_auctions = auction_listings
-        else:
+        else: # if they selected a category
             filtered_auctions = auction_listings[auction_listings['Category'] == selected_category]
 
         print(filtered_auctions)
@@ -144,24 +158,23 @@ def seller(email):
 
     # Get the seller's information from merged_table using their email
     seller_info = df[df['ï»¿email'] == seller_email]
-    print(seller_info)
-    print(df['ï»¿email'])
-    print(seller_email)
 
     # Get all the auctions from auction_listings with the seller's email
     seller_auctions = auction_listings[auction_listings['Seller_Email'] == seller_email]
 
-    if request.method == 'POST':
+    if request.method == 'POST': # if they clicked to remove an auction, change its description
         listing_id = request.form['listing_id']
         print(listing_id)
-        auction_listings.loc[auction_listings['Listing_ID'] == int(listing_id), 'Product_Description'] = 'AUCTION REMOVED'
+        auction_listings.loc[
+            auction_listings['Listing_ID'] == int(listing_id), 'Product_Description'] = 'AUCTION REMOVED'
         return redirect(url_for('seller', email=seller_email))
 
     # Render the seller.html template with the seller's information and auctions
-    return render_template('seller.html', seller_info=seller_info.to_dict('records'), seller_auctions=seller_auctions.to_dict('records'))
+    return render_template('seller.html', seller_info=seller_info.to_dict('records'),
+                           seller_auctions=seller_auctions.to_dict('records'))
 
 
-@app.route('/item/<int:listing_id>')
+@app.route('/item/<int:listing_id>') # if a bidder clicked on a specific item
 def item(listing_id):
     # Filter the auction_listings dataframe to extract the relevant information
     auction = auction_listings.loc[auction_listings['Listing_ID'] == listing_id].iloc[0]
@@ -177,7 +190,7 @@ def item(listing_id):
 
         current_bid = highest_bid_price
 
-    else:
+    else: # if the item has no bids yet
         print("Listing_ID is not found in the bids_df dataset.")
         bidder_email = "No Bidder"
         current_bid = 0
@@ -194,43 +207,42 @@ def item(listing_id):
                            current_bid=current_bid, listing_id=listing_id)
 
 
-@app.route('/submit_bid', methods=['POST'])
+@app.route('/submit_bid', methods=['POST']) # for submitting a bid on their selected item
 def submit_bid():
+    # get all the necessary information to place the bid
     listing_id = request.form['listing_id']
     bid_amount = int(request.form['bid_amount'])
     bidder_email = session['email']
     global bids_df
-    # get the auction they are bidding on so we can abstract relevant information
-    # auction = auction_listings.loc[auction_listings['Listing_ID'] == listing_id].iloc[0]
+    seller_email = bids_df[bids_df['Listing_ID'] == listing_id]['Seller_Email']
 
     # Check if user is the current highest bidder
     current_bid = bids_df.loc[bids_df['Listing_ID'] == listing_id]['Bid_Price'].max()
     print(current_bid)
     print(bids_df['Bidder_Email'])
     print(bids_df.shape[1])
-    # seller_email = auction['Seller_Email']
-    if math.isnan(current_bid) or bids_df['Bidder_Email'].empty:
+    if math.isnan(current_bid) or bids_df['Bidder_Email'].empty: # if there are no bids, we can just place the bid
         print("No Bids on this item")
         # Add new bid to bids_df
-        bids_df.loc[len(bids_df.index)] = [999, "seller_email", listing_id, bidder_email, bid_amount]
+        bids_df.loc[len(bids_df.index)] = [999, seller_email, listing_id, bidder_email, bid_amount]
         # Save the new bid to a CSV file
         bids_df.to_csv('bids.csv', index=False)
 
         flash('Your bid has been submitted!', 'success')
         return redirect(url_for('item', listing_id=listing_id))
 
-    else:
+    else: # if there are bids, we must run a few checks
         print("There are bids on this item")
         current_highest_bidder = bids_df.loc[bids_df['Bid_Price'] == current_bid]['Bidder_Email'].values[0]
-        if bidder_email == current_highest_bidder:
+        if bidder_email == current_highest_bidder: # check to make sure they aren't highest bidder
             flash('You are already the highest bidder for this item!', 'danger')
             return redirect(url_for('item', listing_id=listing_id))
-        elif bid_amount <= current_bid:
+        elif bid_amount <= current_bid: # check to make sure their bid is higher than the last
             flash('Bid amount must be at least 1 unit higher than the current highest bid!', 'danger')
             return redirect(url_for('item', listing_id=listing_id))
-        elif bid_amount > current_bid:
+        elif bid_amount > current_bid: # if conditions met, place the bid
             # Add new bid to bids_df
-            bids_df.loc[len(bids_df.index)] = [999, "seller_email", listing_id, bidder_email, bid_amount]
+            bids_df.loc[len(bids_df.index)] = [999, seller_email, listing_id, bidder_email, bid_amount]
             # Save the new bid to a CSV file
             bids_df.to_csv('bids.csv', index=False)
 
@@ -241,11 +253,13 @@ def submit_bid():
 # view the bids the user has placed
 @app.route('/view_user_bids', methods=['GET', 'POST'])
 def view_user_bids():
+    # displays each bid that is attached to the email they logged in with
     email = session.get('email')
     user_bids_df = bids_df[bids_df['Bidder_Email'] == email]
     user_bids = user_bids_df.to_dict('records')
     return render_template('bid_history.html', user_bids=user_bids)
 
 
+# running the main file
 if __name__ == '__main__':
     app.run(debug=True)
